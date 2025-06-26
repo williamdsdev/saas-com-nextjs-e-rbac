e1a063aaad4eb32152151499a7b263d81b5b0cec
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-06-25 21:04:12 -0300

    feat: implement automatic association by email

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -35,6 +35,15 @@ export async function createAccount(app: FastifyInstance) {
           .send({ message: 'user with same e-mail already exists.' })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
       const passwordHash = await hash(password, 6)
 
       await prisma.user.create({
@@ -42,6 +51,13 @@ export async function createAccount(app: FastifyInstance) {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
 
