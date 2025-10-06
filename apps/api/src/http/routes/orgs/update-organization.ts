c738a5aea8d18d4807e1e7a68aee588db2d62e41
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-10-06 20:02:49 -0300

    feat(org): update organization details

---
+++ apps/api/src/http/routes/orgs/update-organization.ts
@@ -0,0 +1,96 @@
+import { organizationSchema } from '@saas/auth'
+import type { FastifyInstance, FastifyRequest } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
+import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+export async function updateOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Update organization details',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            domain: z.string().nullish(),
+            shouldAttachUsersByDomain: z.boolean().optional(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const req = request as FastifyRequest & {
+          getCurrentUserId: () => Promise<string>
+          getUserMembership: (slug: string) => Promise<{
+            organization: any
+            membership: any
+          }>
+        }
+
+        const { slug } = request.params as { slug: string }
+        const userId = await req.getCurrentUserId()
+        const { membership, organization } = await req.getUserMembership(slug)
+
+        const { name, domain, shouldAttachUsersByDomain } = request.body as {
+          name: string
+          domain?: string
+          shouldAttachUsersByDomain?: boolean
+        }
+
+        const authOrganization = organizationSchema.parse(organization)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', authOrganization)) {
+          throw new UnauthorizedError(
+            `You're not allowed to update this organization.`,
+          )
+        }
+
+        if (domain) {
+          const organizationByDomain = await prisma.organization.findFirst({
+            where: {
+              domain,
+              id: {
+                not: organization.id,
+              },
+            },
+          })
+
+          if (organizationByDomain) {
+            throw new BadRequestError(
+              'Another organization with same domain already exists.',
+            )
+          }
+        }
+
+        await prisma.organization.update({
+          where: {
+            id: organization.id,
+          },
+          data: {
+            name,
+            domain,
+            shouldAttachUsersByDomain,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
