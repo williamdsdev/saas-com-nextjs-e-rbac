98d06d22ca7b3445fbc4a506f0c7d1b8afce6fa1
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-10-06 19:34:05 -0300

    feat(org): add organization creation feature

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -17,6 +17,7 @@ import { errorHandler } from './error-handler'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
+import { createOrganization } from './routes/orgs/create-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -61,6 +62,8 @@ app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
+
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
