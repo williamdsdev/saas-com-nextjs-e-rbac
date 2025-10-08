17cbefdade6f3a4ad548243fde6d2e5cd3437f85
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-10-08 20:45:12 -0300

    feat(project): add project creation feature

---
+++ apps/api/src/http/routes/orgs/create-project.ts
@@ -0,0 +1,77 @@
+import type { FastifyInstance, FastifyRequest } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
+import { prisma } from '@/lib/prisma'
+import { createSlug } from '@/utils/create-slug'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+export async function createProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug/projects',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Create a new project',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            description: z.string(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            201: z.object({
+              projectId: z.string().uuid(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const req = request as FastifyRequest & {
+          getCurrentUserId: () => Promise<string>
+          getUserMembership: (slug: string) => Promise<{
+            organization: any
+            membership: any
+          }>
+        }
+
+        const { slug } = request.params as { slug: string }
+        const userId = await req.getCurrentUserId()
+        const { organization, membership } = await req.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('create', 'Project')) {
+          throw new UnauthorizedError(
+            `You're not allowed to create new projects.`,
+          )
+        }
+
+        const { name, description } = request.body as {
+          name: string
+          description: string
+        }
+
+        const project = await prisma.project.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            description,
+            organizationId: organization.id,
+            ownerId: userId,
+          },
+        })
+
+        return reply.status(201).send({
+          projectId: project.id,
+        })
+      },
+    )
+}
