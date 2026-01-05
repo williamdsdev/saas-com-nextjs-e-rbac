84b54b0a0e076802a2fddd41884cde2395903311
William Desteffani Soares <williamds.uchiha@gmail.com>
2026-01-05 20:05:04 -0300

    feat(org): add invitation creation

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -32,6 +32,7 @@ import { updateProject } from './routes/projects/update-project'
 import { getMembers } from './routes/members/get-members'
 import { updateMember } from './routes/members/update-member'
 import { removeMember } from './routes/members/remove-member'
+import { createInvite } from './routes/invites/create-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -94,6 +95,8 @@ app.register(updateProject)
 app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
+
+app.register(createInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
