7fd8fb348965bd66e4b3c1c7999f277a62505d2a
William Desteffani Soares <williamds.uchiha@gmail.com>
2026-01-05 21:25:03 -0300

    feat(invite): reject invitation

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -36,6 +36,7 @@ import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
 import { acceptInvite } from './routes/invites/accept-invite'
+import { rejectInvite } from './routes/invites/reject-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -103,6 +104,7 @@ app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
 app.register(acceptInvite)
+app.register(rejectInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
